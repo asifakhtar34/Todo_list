28dee963b156e66029790f8e749b9d53ede2f326
asifakhtar <shaikhasifakhtar@gmail.com>
2021-06-25 17:21:06 +0530

    added Local storage access

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+
+fs.writeFile('file.txt', "HI asif from node.js", () => {
+    console.log("everythings good")
+})
--- control.js
+++ control.js
@@ -1,40 +1,56 @@
-const addBtn = document.getElementById("addBtn");
+  const addBtn = document.getElementById("addBtn");
 
-const listContainer = document.getElementById("listContainer");
+  const listContainer = document.getElementById("listContainer");
 
-let listArr = document.querySelectorAll("li");
-let delArr1 = [];
-// console.log(delArr1)
-//to add cross button to list Items
-for (i = 0; i < listArr.length; i++) {
-    let delBtn = document.createElement("div");
-    let text = document.createTextNode("X");
-    delBtn.appendChild(text);
-    delBtn.classList.add("delBtn");
-    listArr[i].appendChild(delBtn);
-}
+  let listArr = document.querySelectorAll("li");
 
-let delBtn = document.getElementsByClassName("delBtn");
+  // body.addEventListener('load', (ev) => {})
+  // alert("hello there! how are you");
 
-//adding new items to list
-function appendItem() {
-    let delBtn = document.createElement("div");
-    let text = document.createTextNode("X");
-    delBtn.appendChild(text);
-    delBtn.classList.add("delBtn");
-    let li = document.createElement("LI");
-    li.appendChild(delBtn);
-    listContainer.appendChild(li);
+  //to add cross button to list Items
+  for (i = 0; i < listArr.length; i++) {
+      let delBtn = document.createElement("div");
+      let text = document.createTextNode("X");
+      delBtn.appendChild(text);
+      delBtn.classList.add("delBtn");
+      listArr[i].appendChild(delBtn);
+  }
 
-    delBtn.addEventListener("click", (ev) => {
-        console.log("hi");
-        console.log(ev.target.parentNode.remove());
-    });
-}
-let delArr = Array.from(delBtn);
+  let delBtn = document.getElementsByClassName("delBtn");
 
-delArr.forEach((item, ind, arr) =>
-    item.addEventListener("click", (ev) => {
-        console.log(ev.target.parentNode.remove());
-    })
-);
+  //adding new items to list
+  function appendItem() {
+      //grabbing the new task from the user
+      const newTask = document.getElementById("newTask").value;
+      // console.log(newTask)
+      let delBtn = document.createElement("div");
+      let text = document.createTextNode("X");
+      delBtn.appendChild(text);
+      delBtn.classList.add("delBtn");
+      let li = document.createElement("LI");
+      li.appendChild(delBtn);
+      li.append(newTask)
+
+      listContainer.appendChild(li);
+
+      delBtn.addEventListener("click", (ev) => {
+          console.log("hi");
+          console.log(ev.target.parentNode.remove());
+      });
+
+      q
+
+  }
+
+  //delete a list-Item  from the list
+  let delArr = Array.from(delBtn);
+  delArr.forEach((item, ind, arr) =>
+      item.addEventListener("click", (ev) => {
+          console.log(ev.target.parentNode.remove());
+      })
+  );
+
+  // store in Local storage
+  function setStorageItem(key, value) {
+      localStorage.setItem(key, value)
+  }
