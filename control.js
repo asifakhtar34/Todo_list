2b7ec186c517b1f126ae18d832bb58301e8503eb
asifakhtar <shaikhasifakhtar@gmail.com>
2021-06-25 18:57:47 +0530

    added edit task functions

--- control.js
+++ control.js
@@ -1,56 +1,93 @@
-  const addBtn = document.getElementById("addBtn");
+const addBtn = document.getElementById("addBtn");
 
-  const listContainer = document.getElementById("listContainer");
+const listContainer = document.getElementById("listContainer");
 
-  let listArr = document.querySelectorAll("li");
+let listArr = document.querySelectorAll("li");
+let inTask = document.getElementById("newTask");
 
-  // body.addEventListener('load', (ev) => {})
-  // alert("hello there! how are you");
+// body.addEventListener('load', (ev) => {})
+// alert("hello there! how are you");
 
-  //to add cross button to list Items
-  for (i = 0; i < listArr.length; i++) {
-      let delBtn = document.createElement("div");
-      let text = document.createTextNode("X");
-      delBtn.appendChild(text);
-      delBtn.classList.add("delBtn");
-      listArr[i].appendChild(delBtn);
-  }
+//to add cross button to list Items
+for (i = 0; i < listArr.length; i++) {
+    let delBtn = document.createElement("div");
+    let text = document.createTextNode("X");
+    let editBtn = document.createElement("div");
+    editBtn.innerHTML = "&#128736;";
+    // editBtn.appendChild(editSymb);
+    delBtn.appendChild(text);
+    delBtn.classList.add("delBtn");
+    editBtn.classList.add("editBtn");
+    listArr[i].appendChild(delBtn);
+    listArr[i].appendChild(editBtn);
+}
 
-  let delBtn = document.getElementsByClassName("delBtn");
+// enter to add item
+inTask.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+        appendItem();
+    }
+});
 
-  //adding new items to list
-  function appendItem() {
-      //grabbing the new task from the user
-      const newTask = document.getElementById("newTask").value;
-      // console.log(newTask)
-      let delBtn = document.createElement("div");
-      let text = document.createTextNode("X");
-      delBtn.appendChild(text);
-      delBtn.classList.add("delBtn");
-      let li = document.createElement("LI");
-      li.appendChild(delBtn);
-      li.append(newTask)
+//adding new items to list
+function appendItem() {
+    //grabbing the new task from the user
+    // console.log(newTask)
+    const newTask = inTask.value;
+    if (newTask == "") {
+        alert("enter task");
+    } else {
+        // adding new task
+        let li = document.createElement("LI");
+        li.append(newTask);
+        // adding del button to each task
+        let delBtn = document.createElement("div");
+        let text = document.createTextNode("X");
+        delBtn.appendChild(text);
+        delBtn.classList.add("delBtn");
+        li.appendChild(delBtn);
+        //adding edit button to each task
+        let editBtn = document.createElement("div");
+        editBtn.innerHTML = "&#128736;";
+        editBtn.classList.add("editBtn");
+        li.appendChild(editBtn);
+        // finally appending the new task to the lists
+        listContainer.appendChild(li);
+        //adding delete event to new task
+        delBtn.addEventListener("click", (ev) => {
+            console.log("hi");
+            console.log(ev.target.parentNode.remove());
+        });
+        //adding edit event to new task
+        editBtn.addEventListener("click", (ev) => {
+            console.log("hi");
+            console.log(ev.target.parentNode.childNodes[0].nodeValue);
+        });
+        // clearing the new Task input
+        inTask.value = "";
+    }
+}
 
-      listContainer.appendChild(li);
+//delete a list-Item  from the list
+let delBtn = document.getElementsByClassName("delBtn");
+let editBtn = document.getElementsByClassName("editBtn");
+let delArr = Array.from(delBtn);
+let editArr = Array.from(editBtn);
+delArr.forEach((item, ind, arr) =>
+    item.addEventListener("click", (ev) => {
+        console.log(ev.target.parentNode.remove());
+    })
+);
+editArr.forEach((item, ind, arr) =>
+    item.addEventListener("click", (ev) => {
+        // add edit functions
 
-      delBtn.addEventListener("click", (ev) => {
-          console.log("hi");
-          console.log(ev.target.parentNode.remove());
-      });
+        //grabbed the task in string
+        console.log(ev.target.parentNode.childNodes[0].nodeValue);
+    })
+);
 
-      q
-
-  }
-
-  //delete a list-Item  from the list
-  let delArr = Array.from(delBtn);
-  delArr.forEach((item, ind, arr) =>
-      item.addEventListener("click", (ev) => {
-          console.log(ev.target.parentNode.remove());
-      })
-  );
-
-  // store in Local storage
-  function setStorageItem(key, value) {
-      localStorage.setItem(key, value)
-  }
+// store in Local storage
+function setStorageItem(key, value) {
+    localStorage.setItem(key, value);
+}
