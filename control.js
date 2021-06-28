be093e9ae0beb0169504f20e6c1ec82323498643
asifakhtar <shaikhasifakhtar@gmail.com>
2021-06-28 14:21:56 +0530

    enabled the edit function

--- control.js
+++ control.js
@@ -1,4 +1,5 @@
 const addBtn = document.getElementById("addBtn");
+const updateBtn = document.getElementById("updateBtn");
 
 const listContainer = document.getElementById("listContainer");
 
@@ -23,11 +24,13 @@ for (i = 0; i < listArr.length; i++) {
 }
 
 // enter to add item
-inTask.addEventListener("keyup", (event) => {
+inTask.addEventListener("keyup", enter);
+
+function enter(event) {
     if (event.key == "Enter") {
         appendItem();
     }
-});
+}
 
 //adding new items to list
 function appendItem() {
@@ -61,11 +64,62 @@ function appendItem() {
         //adding edit event to new task
         editBtn.addEventListener("click", (ev) => {
             console.log("hi");
-            console.log(ev.target.parentNode.childNodes[0].nodeValue);
+            inTask.value = ev.target.parentNode.childNodes[0].nodeValue;
+            let flag = 1;
+            // console.log(ev.target.parentNode.childNodes[0]);
+            console.log(inTask.value);
+            addBtn.disabled = true;
+
+            inTask.removeEventListener("keyup", enter);
+
+            // inTask.addEventListener("keyup", updateListEnter);
+
+            updateBtn.addEventListener("click", updateList);
+
+            function updateList() {
+
+
+                console.log("hello");
+                let uItem = inTask.value;
+                console.log(uItem);
+                if (uItem == "") {
+                    alert("enter some task")
+                } else {
+
+                    ev.target.parentNode.childNodes[0].nodeValue = uItem;
+                    addBtn.disabled = false;
+                    // inTask.removeEventListener("keyup", update);
+
+                    inTask.value = "";
+
+                    updateBtn.removeEventListener("click", updateList);
+                    inTask.addEventListener("keyup", enter);
+                }
+
+
+            }
+
+            // function updateListEnter(event) {
+            //     if (event.key == "Enter") {
+            //         console.log("hello");
+            //         let uItem = inTask.value;
+            //         console.log(uItem);
+
+            //         ev.target.parentNode.childNodes[0].nodeValue = uItem;
+            //         addBtn.disabled = false;
+            //         // inTask.removeEventListener("keyup", update);
+
+            //         inTask.value = "";
+            //         inTask.removeEventListener("keyup", updateListEnter);
+            //     }
+            // }
         });
+
+
+
         // clearing the new Task input
-        inTask.value = "";
     }
+    inTask.value = "";
 }
 
 //delete a list-Item  from the list
@@ -83,7 +137,41 @@ editArr.forEach((item, ind, arr) =>
         // add edit functions
 
         //grabbed the task in string
-        console.log(ev.target.parentNode.childNodes[0].nodeValue);
+        console.log("hi");
+        inTask.value = ev.target.parentNode.childNodes[0].nodeValue;
+        let flag = 1;
+        // console.log(ev.target.parentNode.childNodes[0]);
+        console.log(inTask.value);
+        addBtn.disabled = true;
+
+        inTask.removeEventListener("keyup", enter);
+
+        // inTask.addEventListener("keyup", updateListEnter);
+
+        updateBtn.addEventListener("click", updateList);
+
+        function updateList() {
+
+
+
+
+            console.log("hello");
+            let uItem = inTask.value;
+            console.log(uItem);
+            if (uItem == "") {
+                alert("enter some task")
+            } else {
+
+                ev.target.parentNode.childNodes[0].nodeValue = uItem;
+                addBtn.disabled = false;
+                // inTask.removeEventListener("keyup", update);
+
+                inTask.value = "";
+
+                updateBtn.removeEventListener("click", updateList);
+            }
+
+        }
     })
 );
 
@@ -91,3 +179,36 @@ editArr.forEach((item, ind, arr) =>
 function setStorageItem(key, value) {
     localStorage.setItem(key, value);
 }
+
+// function updateItem(value, item) {
+
+// console.log("hi")
+// inTask.addEventListener("keyup", function update(event) {
+//     let uItem = inTask.value;
+//     if (event.key == "Enter") {
+//         ev.target.parentNode.childNodes[0].nodeValue = uItem;
+//         inTask.value = "";
+//         inTask.removeEventListener("keyup", update);
+
+//     }
+// })
+
+// }
+
+// function updateList(ev) {
+//     console.log("hi");
+//     inTask.value = ev.target.parentNode.childNodes[0].nodeValue;
+//     let flag = 1;
+//     // console.log(ev.target.parentNode.childNodes[0]);
+//     console.log(inTask.value)
+
+//     updateBtn.addEventListener("click", function update(event) {
+//         console.log("hello")
+//         let uItem = inTask.value;
+//         if (event.key == "Enter") {
+//             ev.target.parentNode.childNodes[0].nodeValue = uItem;
+//             inTask.value = "";
+//             // inTask.removeEventListener("keyup", update);
+
+//         }
+//     });
